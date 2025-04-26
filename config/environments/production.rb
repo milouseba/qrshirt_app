c4543e8f6f8d9ea0e4508274e3c70762239c9b80
sebastien milou <milouseba@gmail.com>
2025-04-26 15:59:02 +0200

    add prod url in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,9 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.routes.default_url_options[:host] = ENV['APP_HOST']
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.routes.default_url_options[:host] = ENV['APP_HOST']
 
   # Code is not reloaded between requests.
   config.enable_reloading = false

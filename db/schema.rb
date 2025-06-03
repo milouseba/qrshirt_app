edfebc8bed9c44ef597f2fe6406dcf0bd16143cf
david michel <davidmichel77@gmail.com>
2025-06-03 16:48:03 +0200

    create new order if applicable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_11_173532) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_03_140852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_11_173532) do
     t.integer "quantity"
     t.boolean "paid"
     t.string "paypal_order_id"
+    t.string "shopify_id"
+    t.string "size"
   end
 
   create_table "tshirts", force: :cascade do |t|

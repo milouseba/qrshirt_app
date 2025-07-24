e13db7c3d74ae548546c0a2cf534945517404f62
sebastien milou <milouseba@gmail.com>
2025-07-24 19:29:59 +0200

    fix qr code field

--- app/services/insert_qr_code_in_logo_service.rb
+++ app/services/insert_qr_code_in_logo_service.rb
@@ -26,7 +26,7 @@ class InsertQrCodeInLogoService
       result.write(file.path)
       file.rewind
 
-      order.qr_code_file.attach(
+      order.qr_code_id.attach(
         io: file,
         filename: "logo_with_qr_#{qr_code_id}.png",
         content_type: "image/png"
